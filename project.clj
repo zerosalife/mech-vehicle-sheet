8cb160be84c279b6613d6139559b129c12f58380
zerosalife <zerosalife@gmail.com>
2015-10-23 09:11:12 -0500

    initial commit for clojure

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject mech-vehicle-sheet "0.1.0-SNAPSHOT"
+  :description "A procedural mecha generator"
+  :url "http://www.github.com/zerosalife/mech-vehicle-sheet"
+  :license {:name "Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License"
+            :url "http://creativecommons.org/licenses/by-nc-sa/4.0/deed.en_US"}
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :profiles {:dev {:plugins []
+                   :dependencies []
+                   :source-paths ["dev"]}})
+;;;
