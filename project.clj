28df36ce00e7aa4334d0b3585c17ecb69775a01b
zerosalife <zerosalife@gmail.com>
2015-11-07 15:05:47 -0600

    upgrade clojurescript

--- project.clj
+++ project.clj
@@ -5,13 +5,13 @@
             :url "http://creativecommons.org/licenses/by-nc-sa/4.0/deed.en_US"}
   :main mech.system
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.145"]
+                 [org.clojure/clojurescript "1.7.170"]
                  [http-kit "2.1.18"]
                  [com.stuartsierra/component "0.2.3"]
                  [compojure "1.4.0"]
                  [quiescent "0.2.0-alpha1"]
                  [environ "1.0.1"]]
-  :plugins [[lein-cljsbuild "1.1.0"]]
+  :plugins [[lein-cljsbuild "1.1.1-SNAPSHOT"]]
   :min-lein-version "2.0.0"
   :uberjar-name "mech-vehicle-sheet.jar"
   :profiles {:uberjar {:source-paths ["src"]
@@ -26,7 +26,7 @@
                                                          :pretty-print false}}]}}
 
              :dev {:plugins [[cider/cider-nrepl "0.8.2"]
-                             [lein-figwheel "0.4.1"]]
+                             [lein-figwheel "0.5.0-SNAPSHOT"]]
                    :dependencies [[reloaded.repl "0.1.0"]]
                    :source-paths ["dev"]
                    :cljsbuild {:builds [{:source-paths ["src" "dev"]
