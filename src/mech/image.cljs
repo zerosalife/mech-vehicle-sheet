461e0216b707a60d37802ea15eb072fa3fd3f770
zerosalife <zerosalife@gmail.com>
2015-11-30 12:41:17 -0600

    Add correct placeholder image coordinates

--- src/mech/image.cljs
+++ src/mech/image.cljs
@@ -9,33 +9,33 @@
   (assoc image
          :torso {:schema-x 4
                  :schema-y 3
-                 :schema-width 6
-                 :schema-height 7
+                 :schema-width 7
+                 :schema-height 6
                  :color "chartreuse"
                  :title "Torso"}))
 
 (defn legs [image]
   (assoc image
-         :legs {:schema-x 4
-                :schema-y 8
-                :schema-width 6
-                :schema-height 7
+         :legs {:schema-x 2
+                :schema-y 9
+                :schema-width 11
+                :schema-height 16
                 :color "goldenrod"
                 :title "Legs"}))
 
 (defn left-shoulder [image]
   (assoc image
-         :left-shoulder {:schema-x 10
-                         :schema-y 4
-                         :schema-width 6
+         :left-shoulder {:schema-x 11
+                         :schema-y 2
+                         :schema-width 3
                          :schema-height 3
                          :color "honeydew"
                          :title "Left Shoulder"}))
 
 (defn right-shoulder [image]
   (assoc image
-         :right-shoulder {:schema-x 0
-                         :schema-y 4
+         :right-shoulder {:schema-x 1
+                         :schema-y 2
                          :schema-width 3
                          :schema-height 3
                          :color "powderblue"
@@ -43,27 +43,27 @@
 
 (defn left-arm [image]
   (assoc image
-         :left-arm {:schema-x 10
-                    :schema-y 7
-                    :schema-width 6
-                    :schema-height 3
+         :left-arm {:schema-x 12
+                    :schema-y 5
+                    :schema-width 3
+                    :schema-height 8
                     :color "indianred"
                     :title "Left Arm"}))
 
 (defn right-arm [image]
   (assoc image
          :right-arm {:schema-x 0
-                     :schema-y 7
+                     :schema-y 5
                      :schema-width 3
-                     :schema-height 3
+                     :schema-height 8
                      :color "magenta"
                      :title "Right Arm"}))
 
 (defn head [image]
   (assoc image
-         :head {:schema-x 6
+         :head {:schema-x 5
                 :schema-y 0
-                :schema-width 2
+                :schema-width 5
                 :schema-height 4
                 :color "cyan"
                 :title "Head"}))
